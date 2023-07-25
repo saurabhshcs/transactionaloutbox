641ea3a18ba5ad7fbb6511fa8fc45720b7edbc70
saurabhshcs <saurabhshcs@gmail.com>
2023-07-25 12:17:16 +0100

    saurabhshcs:Initial code structure commit.

---
+++ src/main/java/com/saurabhshcs/transactionaloutbox/TransactionaloutboxApplication.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.transactionaloutbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionaloutboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionaloutboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabhshcs/transactionaloutbox/TransactionaloutboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.transactionaloutbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionaloutboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
